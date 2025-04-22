aba2a9d312e8a0eb151d9d511085c98bf48fc582
Razeen515 <razeenpatron695@gmail.com>
2025-04-22 11:22:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Mern-doctor-appointment/', // Set the base path for GitHub Pages
 })
